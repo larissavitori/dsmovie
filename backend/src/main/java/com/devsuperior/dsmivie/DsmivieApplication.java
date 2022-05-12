ac58403764e3cf5e5820f2920fdb20f80ec09f3d
Larissa <laryterra@gmail.com>
2022-05-11 21:28:45 -0300

    Projeto criado

---
+++ backend/src/main/java/com/devsuperior/dsmivie/DsmivieApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmivie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmivieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmivieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsmivie/DsmivieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmivie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmivieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
