2c2af7ea102e2a9871f3f78e5d35cba0a2fee4d4
Larissa <laryterra@gmail.com>
2022-05-13 21:02:15 -0300

    remove pastas backend e frontend

---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,38 @@
+import MovieCard from "components/MovieCard";
+import Pagination from "components/Pagination";
+
+function Listing() {
+    return (
+        <>
+            <Pagination />
+
+            <div className="Container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+
+            </div>
+
+        </>
+
+    );
+}
+
+export default Listing;
